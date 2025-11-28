6f95af16e0af92b784c32e3d3974f9eb1b0f5051
LeoLooser <champ@Kali.champ>
2025-11-28 14:16:09 +0500

    7th commit

--- infra/provider.tf
+++ infra/provider.tf
@@ -1,12 +1,2 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = ">= 5.0"
-    }
-  }
-}
-
-provider "aws" {
-  region = var.aws_region
-}
+# (Intentionally left blank)
+# Required providers and aws provider are declared in main.tf only.
